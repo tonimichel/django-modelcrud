b9f26b121322c8d5b4439d9947f0506ee1d5fe5b
Christine Haag <xgwshaa@gws.ms>
2016-05-20 14:32:11 +0200

    add Relationship lookup in list_display

--- modelcrud/templatetags/modelcrud_tags.py
+++ modelcrud/templatetags/modelcrud_tags.py
@@ -1,5 +1,6 @@
 from django import template
 import types
+from django.core.exceptions import FieldDoesNotExist
 
 register = template.Library()
 
@@ -10,37 +11,56 @@ def render_list(obj, object_list):
     crudconf.list_display into account.
     """
     head = []
-    for fname in obj.crudconf.get_list_display():
-        if fname in [f.name for f in type(obj)._meta.get_fields()]:
-            head.append(dict(
-                name=fname,
-                label=type(obj)._meta.get_field(fname).verbose_name
-            ))
+    list_display = obj.crudconf.get_list_display()
+    for fname in list_display:
+
+
+        if '__' in fname:
+            rel_attr = fname.split('__')[1]
+            rel_field_name = fname.split('__')[0]
+            field = type(obj)._meta.get_field(rel_field_name)
+            rel_field = field.rel.to._meta.get_field(rel_attr)
+            label=rel_field.verbose_name
         else:
             try:
-                """
-                try the fieldname foo from getter method get_foo_display()
-                """
-                head.append(dict(
-                    name=fname,
-                    label=type(obj)._meta.get_field(fname.replace('get_','').replace('_display','')).verbose_name
-                ))
-            except:
-                head.append(dict(
-                    name=fname,
-                    label='unkown'
-                ))
+                label=type(obj)._meta.get_field(fname).verbose_name
+            except FieldDoesNotExist as e:
+
+                getter = getattr(obj, fname)
+                label = getattr(getter, 'short_description', fname)
+
+        head.append(dict(
+            name=fname,
+            label=label
+        ))
 
 
     rows = []
-    for entry in object_list:
-        rows.append(
-            dict(
-                obj=entry,
-                values=[getattr(entry, fname)() if isinstance(getattr(entry, fname), types.MethodType) \
-                    else getattr(entry, fname) for fname in obj.crudconf.get_list_display()]
-            )
+    for i, entry in enumerate(object_list):
+        row = dict(
+            obj=entry,
+            values=[]
         )
+        for field_name in list_display:
+
+            if '__' in field_name:
+
+                rel_name = field_name.split('__')[0]
+                rel_attr = field_name.split('__')[1]
+
+                obj = getattr(entry, rel_name)
+                value = getattr(obj, rel_attr)
+
+                row['values'].append(value)
+            else:
+                row['values'].append(getattr(entry, field_name)() if isinstance(getattr(entry, field_name), types.MethodType) \
+                    else getattr(entry, field_name))
+
+        rows.append(row)
+
+
+
+
     return dict(
         head=head,
         rows=rows
